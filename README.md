9b008e094167b487a0b9b1ec01af3f48ab7659e5
Tim Jones <tim.jones@meltwater.com>
2016-09-05 08:47:11 -0400

    feat: Adding second TMP36 sensor and powering off digital pins for less power consumption when not sending data

--- README.md
+++ README.md
@@ -4,21 +4,24 @@ Photon library for managing sensors in my solar kiln
 # Usage
 
 RHT03 Wiring:
-1. Pin 1 goes to 3v3
+1. Pin 1 goes to a digital pin (D0-D6) for input power
 2. Pin 2 goes to a digital input pin (D0-D6)
 3. Pin 3 goes nowhere
 4. Pin 4 goes to GND
 
 TMP36 Wiring:
-1. Pin 1 goes to 3v3
+1. Pin 1 goes to the same digital pin that was selected for RHT03
 2. Pin 2 goes to an analog input pin (A0-A5)
 3. Pin 3 goes to GND
+
+The second TMP36 should be wired up the same way, but using a different analog input pin
 
 For the sensor setup:
 
 1. Change the RHT03_DATA_PIN value in kiln.ino to the digital pin you connected it to in the previous set of steps
-2. Do the same for the TMP36_DATA_PIN value
-3. Flash your photon!
+2. Do the same for the TMP36_1_DATA_PIN and TMP36_2_DATA_PIN value
+3. Set POWER_PIN to the digital pin that was selected for input power for the RHT03 and TMP36 sensors
+4. Flash your photon!
 
 For the ThingSpeak/ThingTweet integrations:
 
--- kiln.ino
+++ kiln.ino
@@ -4,13 +4,16 @@
 #include "twitter_speak.h"
 #include "private_data.h"
 
-const int RHT03_DATA_PIN = D6; // RHT03 data pin
-const int TMP36_DATA_PIN = A0;
+const int RHT03_DATA_PIN = D6;
+const int TMP36_1_DATA_PIN = A0;
+const int TMP36_2_DATA_PIN = A1;
+const int POWER_PIN = D0;
 
 enum ThingSpeakFields {
 	THING_SPEAK_RHT03_HUMIDITY_FIELD = 1,
 	THING_SPEAK_RHT03_TEMPERATURE_FIELD,
-	THING_SPEAK_TMP36_TEMPERATURE_FIELD
+	THING_SPEAK_TMP36_1_TEMPERATURE_FIELD,
+	THING_SPEAK_TMP36_2_TEMPERATURE_FIELD
 };
 
 const unsigned short THING_SPEAK_SUCCESS = 200;
@@ -22,7 +25,8 @@ const int WAKEUP_DELAY = 2000;
 const int PRE_SLEEP_DELAY = 500;
 
 RHT03 rht03;
-TMP36 tmp36;
+TMP36 tmp36_1;
+TMP36 tmp36_2;
 
 ThingSpeakClass thingSpeak;
 TCPClient client;
@@ -35,25 +39,31 @@ void setup() {
 	thingSpeak.begin(client);
 	twitterSpeak.begin(THING_TWEET_API_KEY);
 
-	tmp36.begin(TMP36_DATA_PIN);
+	tmp36_1.begin(TMP36_1_DATA_PIN);
+	tmp36_2.begin(TMP36_2_DATA_PIN);
 	rht03.begin(RHT03_DATA_PIN);
-
-	delay(WAKEUP_DELAY);
 }
 
 void loop() {
 
-	int tmp36Result = tmp36.update();
+	wakeUp();
+
+	int tmp36_1_Result = tmp36_1.update();
+	int tmp36_2_Result = tmp36_2.update();
 	int rht03Result = rht03.update();
 
-	if(tmp36Result && rht03Result) {
+	if(tmp36_1_Result && tmp36_2_Result && rht03Result) {
 		Serial.println("RHT03 Humidity: " + String(rht03.humidity(), 2) + " %");
 		Serial.println("RHT03 Temp (F): " + String(rht03.tempF(), 2) + " deg F");
 		Serial.println("RHT03 Temp (C): " + String(rht03.tempC(), 2) + " deg C");
 
-		Serial.println("TMP36 Volts: " + String(tmp36.volts(), 2));
-		Serial.println("TMP36 Temp (C): " + String(tmp36.tempC(), 2) + " deg C");
-		Serial.println("TMP36 Temp (F): " + String(tmp36.tempF(), 2) + " deg F");
+		Serial.println("TMP36(1) Volts: " + String(tmp36_1.volts(), 2));
+		Serial.println("TMP36(1) Temp (C): " + String(tmp36_1.tempC(), 2) + " deg C");
+		Serial.println("TMP36(1) Temp (F): " + String(tmp36_1.tempF(), 2) + " deg F");
+
+		Serial.println("TMP36(2) Volts: " + String(tmp36_2.volts(), 2));
+		Serial.println("TMP36(2) Temp (C): " + String(tmp36_2.tempC(), 2) + " deg C");
+		Serial.println("TMP36(2) Temp (F): " + String(tmp36_2.tempF(), 2) + " deg F");
 
 		if(!publishEvent()) {
 			Serial.println("Unable to publish temp event!");
@@ -61,13 +71,28 @@ void loop() {
 			// if we were successful, go to sleep for a while to save battery life.
 			// this will reset the entire program so we start up from the beginning
 			delay(PRE_SLEEP_DELAY);
-			//System.sleep(SLEEP_MODE_DEEP, MEASUREMENT_INTERVAL);
+			sleep(MEASUREMENT_INTERVAL);
 		}
+	} else {
+		Serial.println("Oh noes!  Couldn't read from our sensors!");
 	}
 
 	// we'll only get here if something went wrong.  wait a little while before
 	// we try again
 	delay(FAILURE_INTERVAL);
+}
+
+void wakeUp() {
+	pinMode(POWER_PIN, OUTPUT);
+	digitalWrite(POWER_PIN, HIGH);
+
+	delay(WAKEUP_DELAY);
+}
+
+void sleep(int seconds) {
+	digitalWrite(POWER_PIN, LOW);
+	digitalWrite(POWER_PIN, LOW);
+	//System.sleep(SLEEP_MODE_DEEP, seconds);
 }
 
 bool publishEvent() {
@@ -84,7 +109,8 @@ bool publishEvent() {
 
 	thingSpeak.setField(THING_SPEAK_RHT03_HUMIDITY_FIELD, rht03.humidity());
 	thingSpeak.setField(THING_SPEAK_RHT03_TEMPERATURE_FIELD, rht03.tempF());
-	thingSpeak.setField(THING_SPEAK_TMP36_TEMPERATURE_FIELD, tmp36.tempF());
+	thingSpeak.setField(THING_SPEAK_TMP36_1_TEMPERATURE_FIELD, tmp36_1.tempF());
+	thingSpeak.setField(THING_SPEAK_TMP36_2_TEMPERATURE_FIELD, tmp36_2.tempF());
 
 	thingSpeakResult = thingSpeak.writeFields(THING_SPEAK_CHANNEL_ID, THING_SPEAK_API_KEY);
 	if(thingSpeakResult != THING_SPEAK_SUCCESS) {
